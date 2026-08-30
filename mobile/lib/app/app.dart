ba88efcb7d08a83a05cc18fbe67dbc775ef3d89e
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-08-30 23:11:23 +0100

    Connect task store to app shell

--- mobile/lib/app/app.dart
+++ mobile/lib/app/app.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'theme/app_theme.dart';
+import '../features/tasks/application/task_store.dart';
+import '../features/tasks/presentation/add_task_page.dart';
 import '../features/tasks/presentation/today_page.dart';
 
-class TodoApp extends StatelessWidget {
+class TodoApp extends StatefulWidget {
   const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  final TaskStore _taskStore = TaskStore();
+
+  @override
+  void dispose() {
+    _taskStore.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +29,18 @@ class TodoApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
-      home: const TodayPage(),
+      home: TodayPage(store: _taskStore),
     );
+  }
+}
+
+class TodoAppShell extends StatelessWidget {
+  const TodoAppShell({super.key, required this.store});
+
+  final TaskStore store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TodayPage(store: store);
   }
 }
