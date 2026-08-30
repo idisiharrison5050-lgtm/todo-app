a2bba842d6e6f568745ca718b1d737fe49a16790
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-08-30 15:27:04 +0100

    Add task reminder relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -23,4 +23,9 @@ class Task extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function reminders()
+    {
+        return $this->hasMany(Reminder::class);
+    }
 }
