eb37141a8b91c315049df229a20d894071a9f0c1
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-08-30 14:58:46 +0100

    Add todo routes

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::delete('/tasks/completed/clear', [TaskController::class, 'clearCompleted'])->name('tasks.clear-completed');
