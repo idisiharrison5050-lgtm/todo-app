6ad1eefbb8aa9cf4a5b6bd36fa3f60c2c13ef07e
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-08-31 15:58:00 +0100

    Open task details from reminder notifications

--- mobile/lib/app/app.dart
+++ mobile/lib/app/app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'theme/app_theme.dart';
+import '../features/reminders/data/local_notification_service.dart';
 import '../features/tasks/application/task_store.dart';
+import '../features/tasks/domain/task.dart';
 import '../features/tasks/presentation/home_page.dart';
+import '../features/tasks/presentation/task_detail_page.dart';
 
 class TodoApp extends StatefulWidget {
   const TodoApp({super.key});
@@ -14,16 +17,29 @@ class TodoApp extends StatefulWidget {
 class _TodoAppState extends State<TodoApp> {
   late final TaskStore _taskStore;
   late final Future<void> _loadFuture;
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
 
   @override
   void initState() {
     super.initState();
     _taskStore = TaskStore();
+    LocalNotificationService.onNotificationTap = _openTaskFromNotification;
     _loadFuture = _taskStore.load();
+  }
+
+  void _openTaskFromNotification(String taskId) {
+    final context = _navigatorKey.currentContext;
+    if (context == null) return;
+    final matches = _taskStore.tasks.where((task) => task.id == taskId);
+    if (matches.isEmpty) return;
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) => TaskDetailPage(store: _taskStore, task: matches.first),
+    ));
   }
 
   @override
   void dispose() {
+    LocalNotificationService.onNotificationTap = null;
     _taskStore.dispose();
     super.dispose();
   }
@@ -31,6 +47,7 @@ class _TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigatorKey,
       title: 'Todo',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
