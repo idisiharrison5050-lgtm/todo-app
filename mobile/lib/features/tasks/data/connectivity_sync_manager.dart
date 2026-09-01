4ddacb19efecdd7170b2da06254311e6020fae74
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-01 13:32:50 +0100

    Add automatic connectivity-triggered sync manager

---
+++ mobile/lib/features/tasks/data/connectivity_sync_manager.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+import 'syncing_task_repository.dart';
+
+class ConnectivitySyncManager {
+  ConnectivitySyncManager(this._repository);
+
+  final SyncingTaskRepository _repository;
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
+  bool _started = false;
+
+  void start() {
+    if (_started) return;
+    _started = true;
+    _subscription = Connectivity().onConnectivityChanged.listen((results) {
+      if (results.any((result) => result != ConnectivityResult.none)) {
+        unawaited(_repository.syncNow());
+      }
+    });
+  }
+
+  Future<void> dispose() async {
+    await _subscription?.cancel();
+    _subscription = null;
+    _started = false;
+  }
+}
