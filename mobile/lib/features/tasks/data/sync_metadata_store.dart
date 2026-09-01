dc3fad25af2068137556f6ba9584e80975935f42
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-01 16:51:51 +0100

    Track pending offline task operations

--- mobile/lib/features/tasks/data/sync_metadata_store.dart
+++ mobile/lib/features/tasks/data/sync_metadata_store.dart
@@ -1,5 +1,17 @@
+enum PendingOperationType { upsert, delete }
+
+class PendingOperation {
+  const PendingOperation({required this.id, required this.type, required this.updatedAt});
+  final String id;
+  final PendingOperationType type;
+  final DateTime updatedAt;
+}
+
 abstract interface class SyncMetadataStore {
-  Future<List<String>> getPendingDeletes();
+  Future<List<PendingOperation>> getPendingOperations();
+  Future<void> markPendingUpsert(String id, DateTime updatedAt);
   Future<void> addPendingDelete(String id);
+  Future<void> clearPendingOperation(String id);
+  Future<List<String>> getPendingDeletes();
   Future<void> clearPendingDelete(String id);
 }
