394a5ef0dfc516ac9dad464100ce0a3999d57ff9
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-04 09:11:01 +0100

    Add conservative sync retry loop

--- mobile/lib/features/tasks/data/connectivity_sync_manager.dart
+++ mobile/lib/features/tasks/data/connectivity_sync_manager.dart
@@ -9,21 +9,23 @@ class ConnectivitySyncManager {
 
   final SyncingTaskRepository _repository;
   StreamSubscription<List<ConnectivityResult>>? _subscription;
+  Timer? _retryTimer;
   bool _started = false;
 
   void start() {
     if (_started) return;
     _started = true;
     _subscription = Connectivity().onConnectivityChanged.listen((results) {
-      if (results.any((result) => result != ConnectivityResult.none)) {
-        unawaited(_repository.syncNow());
-      }
+      if (results.any((result) => result != ConnectivityResult.none)) unawaited(_repository.syncNow());
     });
+    _retryTimer = Timer.periodic(const Duration(minutes: 5), (_) => unawaited(_repository.syncNow()));
   }
 
   Future<void> dispose() async {
     await _subscription?.cancel();
     _subscription = null;
+    _retryTimer?.cancel();
+    _retryTimer = null;
     _started = false;
   }
 }
