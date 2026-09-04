c463a8e8a079ee3dc6a86901cb56c66164b6ae84
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-04 09:06:58 +0100

    Harden API rate limits

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,23 @@ use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/register', [AuthController::class, 'register'])->middleware('throttle:6,1');
     Route::post('/auth/login', [AuthController::class, 'login'])->middleware('throttle:6,1');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/auth/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
 
-        Route::get('/tasks', [TaskController::class, 'index']);
-        Route::get('/tasks/deleted', [TaskController::class, 'deleted']);
-        Route::post('/tasks', [TaskController::class, 'store']);
-        Route::delete('/tasks/by-client/{clientId}', [TaskController::class, 'destroyByClientId']);
-        Route::patch('/tasks/{task}', [TaskController::class, 'update']);
-        Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+        Route::get('/tasks', [TaskController::class, 'index'])->middleware('throttle:120,1');
+        Route::get('/tasks/deleted', [TaskController::class, 'deleted'])->middleware('throttle:120,1');
+        Route::post('/tasks', [TaskController::class, 'store'])->middleware('throttle:120,1');
+        Route::delete('/tasks/by-client/{clientId}', [TaskController::class, 'destroyByClientId'])->middleware('throttle:120,1');
+        Route::patch('/tasks/{task}', [TaskController::class, 'update'])->middleware('throttle:120,1');
+        Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->middleware('throttle:120,1');
 
-        Route::get('/reminders', [ReminderController::class, 'index']);
-        Route::post('/reminders', [ReminderController::class, 'store']);
-        Route::patch('/reminders/{reminder}', [ReminderController::class, 'update']);
-        Route::delete('/reminders/{reminder}', [ReminderController::class, 'destroy']);
+        Route::get('/reminders', [ReminderController::class, 'index'])->middleware('throttle:120,1');
+        Route::post('/reminders', [ReminderController::class, 'store'])->middleware('throttle:120,1');
+        Route::patch('/reminders/{reminder}', [ReminderController::class, 'update'])->middleware('throttle:120,1');
+        Route::delete('/reminders/{reminder}', [ReminderController::class, 'destroy'])->middleware('throttle:120,1');
     });
 });
