18755768efdddb0ecbd30ca290b66b58f3557a9a
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-04 10:06:35 +0100

    Apply idempotency protection to mutations

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,14 @@ Route::prefix('v1')->group(function () {
 
         Route::get('/tasks', [TaskController::class, 'index'])->middleware('throttle:120,1');
         Route::get('/tasks/deleted', [TaskController::class, 'deleted'])->middleware('throttle:120,1');
-        Route::post('/tasks', [TaskController::class, 'store'])->middleware('throttle:120,1');
-        Route::delete('/tasks/by-client/{clientId}', [TaskController::class, 'destroyByClientId'])->middleware('throttle:120,1');
-        Route::patch('/tasks/{task}', [TaskController::class, 'update'])->middleware('throttle:120,1');
-        Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->middleware('throttle:120,1');
+        Route::post('/tasks', [TaskController::class, 'store'])->middleware(['throttle:120,1', 'idempotency']);
+        Route::delete('/tasks/by-client/{clientId}', [TaskController::class, 'destroyByClientId'])->middleware(['throttle:120,1', 'idempotency']);
+        Route::patch('/tasks/{task}', [TaskController::class, 'update'])->middleware(['throttle:120,1', 'idempotency']);
+        Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->middleware(['throttle:120,1', 'idempotency']);
 
         Route::get('/reminders', [ReminderController::class, 'index'])->middleware('throttle:120,1');
-        Route::post('/reminders', [ReminderController::class, 'store'])->middleware('throttle:120,1');
-        Route::patch('/reminders/{reminder}', [ReminderController::class, 'update'])->middleware('throttle:120,1');
-        Route::delete('/reminders/{reminder}', [ReminderController::class, 'destroy'])->middleware('throttle:120,1');
+        Route::post('/reminders', [ReminderController::class, 'store'])->middleware(['throttle:120,1', 'idempotency']);
+        Route::patch('/reminders/{reminder}', [ReminderController::class, 'update'])->middleware(['throttle:120,1', 'idempotency']);
+        Route::delete('/reminders/{reminder}', [ReminderController::class, 'destroy'])->middleware(['throttle:120,1', 'idempotency']);
     });
 });
