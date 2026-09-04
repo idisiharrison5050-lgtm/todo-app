c32a5eb5911aa240706c42f477696fb136e94b1f
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-04 10:06:46 +0100

    Persist operation identifiers in sync metadata

--- mobile/lib/features/tasks/data/sync_metadata_store.dart
+++ mobile/lib/features/tasks/data/sync_metadata_store.dart
@@ -1,17 +1,19 @@
 enum PendingOperationType { upsert, delete }
 
 class PendingOperation {
-  const PendingOperation({required this.id, required this.type, required this.updatedAt});
+  const PendingOperation({required this.id, required this.type, required this.updatedAt, this.operationId});
   final String id;
   final PendingOperationType type;
   final DateTime updatedAt;
+  final String? operationId;
 }
 
 abstract interface class SyncMetadataStore {
   Future<List<PendingOperation>> getPendingOperations();
-  Future<void> markPendingUpsert(String id, DateTime updatedAt);
-  Future<void> addPendingDelete(String id);
+  Future<void> markPendingUpsert(String id, DateTime updatedAt, {String? operationId});
+  Future<void> addPendingDelete(String id, {String? operationId});
   Future<void> clearPendingOperation(String id);
   Future<List<String>> getPendingDeletes();
+  Future<String?> getPendingDeleteOperationId(String id);
   Future<void> clearPendingDelete(String id);
 }
